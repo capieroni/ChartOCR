c7873a5423f2ec2dba3afca8a700feb0bfdbb75e
capieroni <158221332+capieroni@users.noreply.github.com>
2024-11-19 11:46:08 -0600

    Replaced cpp files that were not working

--- models/py_utils/_cpools/src/left_pool.cpp
+++ models/py_utils/_cpools/src/left_pool.cpp
@@ -1,61 +1,48 @@
 #include <torch/torch.h>
-
 #include <vector>
 
-std::vector<at::Tensor> pool_forward(
-    at::Tensor input
-) {
-    // Initialize output
+std::vector<at::Tensor> pool_forward(at::Tensor input) {
     at::Tensor output = at::zeros_like(input);
 
-    // Get width
     int64_t width = input.size(3);
-
-    // Copy the last column
-    at::Tensor input_temp  = input.select(3, width - 1);
+    at::Tensor input_temp = input.select(3, width - 1);
     at::Tensor output_temp = output.select(3, width - 1);
     output_temp.copy_(input_temp);
 
     at::Tensor max_temp;
     for (int64_t ind = 1; ind < width; ++ind) {
-        input_temp  = input.select(3, width - ind - 1);
+        input_temp = input.select(3, width - ind - 1);
         output_temp = output.select(3, width - ind);
-        max_temp    = output.select(3, width - ind - 1);
+        max_temp = output.select(3, width - ind - 1);
 
         at::max_out(max_temp, input_temp, output_temp);
     }
 
-    return { 
-        output
-    };
+    return {output};
 }
 
-std::vector<at::Tensor> pool_backward(
-    at::Tensor input,
-    at::Tensor grad_output
-) {
+std::vector<at::Tensor> pool_backward(at::Tensor input, at::Tensor grad_output) {
     auto output = at::zeros_like(input);
 
-    int32_t batch   = input.size(0);
+    int32_t batch = input.size(0);
     int32_t channel = input.size(1);
-    int32_t height  = input.size(2);
-    int32_t width   = input.size(3);
+    int32_t height = input.size(2);
+    int32_t width = input.size(3);
 
-    auto max_val = at::zeros(torch::CUDA(at::kFloat), {batch, channel, height});
-    auto max_ind = at::zeros(torch::CUDA(at::kLong),  {batch, channel, height});
+    auto max_val = at::zeros({batch, channel, height}, at::TensorOptions().dtype(at::kFloat).device(at::kCUDA));
+    auto max_ind = at::zeros({batch, channel, height}, at::TensorOptions().dtype(at::kLong).device(at::kCUDA));
 
     auto input_temp = input.select(3, width - 1);
     max_val.copy_(input_temp);
-
     max_ind.fill_(width - 1);
 
-    auto output_temp      = output.select(3, width - 1);
+    auto output_temp = output.select(3, width - 1);
     auto grad_output_temp = grad_output.select(3, width - 1);
     output_temp.copy_(grad_output_temp);
 
     auto un_max_ind = max_ind.unsqueeze(3);
-    auto gt_mask    = at::zeros(torch::CUDA(at::kByte),  {batch, channel, height});
-    auto max_temp   = at::zeros(torch::CUDA(at::kFloat), {batch, channel, height});
+    auto gt_mask = at::zeros({batch, channel, height}, at::TensorOptions().dtype(at::kByte).device(at::kCUDA));
+    auto max_temp = at::zeros({batch, channel, height}, at::TensorOptions().dtype(at::kFloat).device(at::kCUDA));
     for (int32_t ind = 1; ind < width; ++ind) {
         input_temp = input.select(3, width - ind - 1);
         at::gt_out(gt_mask, input_temp, max_val);
@@ -68,18 +55,10 @@ std::vector<at::Tensor> pool_backward(
         output.scatter_add_(3, un_max_ind, grad_output_temp);
     }
 
-    return {
-        output
-    };
+    return {output};
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def(
-        "forward", &pool_forward, "Left Pool Forward", 
-        py::call_guard<py::gil_scoped_release>()
-    );
-    m.def(
-        "backward", &pool_backward, "Left Pool Backward", 
-        py::call_guard<py::gil_scoped_release>()
-    );
+    m.def("forward", &pool_forward, "Left Pool Forward", py::call_guard<py::gil_scoped_release>());
+    m.def("backward", &pool_backward, "Left Pool Backward", py::call_guard<py::gil_scoped_release>());
 }
